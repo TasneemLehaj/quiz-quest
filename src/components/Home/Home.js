6702aac1062dafceccffe794becf4b90c58a6f3d
TasneemLehaj <tasneemlehaj20@gmail.com>
2022-10-26 15:06:08 +0600

    header added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Topic from '../Topic/Topic';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Home</h2>
+        <div className='home'>
+            <h2>Home
+            </h2>
+            <img src="https://img.freepik.com/free-vector/question-3d-effect-background_52683-30326.jpg?w=900&t=st=1666728063~exp=1666728663~hmac=3f6a8ca449721b065a4c8483ae36c92136a84c425c0f7f75a25f3b07fa264d86" alt="" />
+
         </div>
-    );
+    )
 };
 
 export default Home;
